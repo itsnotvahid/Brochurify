c0d8464420279ab7c5acb28b5b5e744958c31a3c
vahid <itsnotvahid@gmail.com>
2025-01-17 01:14:08 +0330

    added run.sh

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.include_router(router)
 
 
 if __name__ == '__main__':
-    host = os.getenv("HOST", "127.101.43.41")
-    port = int(os.getenv("PORT", 8329))
-    uvicorn.run("main:app", host="127.101.43.41", port=port,
+    host = os.getenv("SOCKET_HOST", "127.101.43.41")
+    port = int(os.getenv("SOCKET_PORT", 8329))
+    uvicorn.run("main:app", host=host, port=port,
                 reload=True)
